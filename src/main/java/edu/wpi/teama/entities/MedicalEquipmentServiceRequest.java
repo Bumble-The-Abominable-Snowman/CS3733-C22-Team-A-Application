5b74c2e42da93318f89b6e1397af25f156a34ba0
Alexander Sun <technoal9@gmail.com>
2022-01-31 20:45:01 -0500

    setup entity medical

---
+++ src/main/java/edu/wpi/teama/entities/MedicalEquipmentServiceRequest.java
@@ -0,0 +1,24 @@
+package edu.wpi.teama.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MedicalEquipmentServiceRequest {
+	enum EquipmentType {
+		BED,
+		XRAY,
+		INFUSION_PUMP,
+		PATIENT_RECLINER,
+	}
+
+	private EquipmentType equipmentType;
+	private String toLocation;
+	private String fromLocation;
+	private String specialNote;
+	private Status status;
+
+}
