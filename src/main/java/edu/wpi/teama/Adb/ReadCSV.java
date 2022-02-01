eb844cee0864af014242665d54767c50aec93c53
NYXXMEISTER <55163966+NYXXMEISTER@users.noreply.github.com>
2022-02-01 18:26:09 -0500

    Update on Read

--- src/main/java/edu/wpi/teama/Adb/ReadCSV.java
+++ src/main/java/edu/wpi/teama/Adb/ReadCSV.java
@@ -1,24 +1,30 @@
 package edu.wpi.teama.Adb;
 
+import edu.wpi.teama.Adb.Employee.Employee;
 import edu.wpi.teama.Adb.Location.Location;
+import edu.wpi.teama.Adb.MedicalEquipment.MedicalEquipment;
+import edu.wpi.teama.Adb.MedicalEquipmentServiceRequest.MedicalEquipmentServiceRequest;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class ReadCSV {
 
-  public static List<Location> readCSV() throws IOException {
+  //Read from Location CSV
+  public static List<Location> readLocationCSV() throws IOException {
     // System.out.println("beginning to read csv");
 
     Scanner lineScanner =
-        new Scanner(ReadCSV.class.getClassLoader().getResourceAsStream("TowerLocations.csv"));
+            new Scanner(ReadCSV.class.getClassLoader().getResourceAsStream("TowerLocations.csv"));
     Scanner dataScanner;
     int dataIndex = 0;
     int lineIndex = 0;
     int intData = 0;
-    List<Location> locList = new ArrayList<>();
+    List<Location> list = new ArrayList<>();
     lineScanner.nextLine();
 
     while (lineScanner.hasNextLine()) { // Scan CSV line by line
@@ -47,13 +53,159 @@ public class ReadCSV {
       }
 
       dataIndex = 0;
-      locList.add(thisLocation);
+      list.add(thisLocation);
       // System.out.println(thisLocation);
 
     }
 
     lineIndex++;
     lineScanner.close();
-    return locList;
+    return list;
   }
+
+  //Read From Employees CSV
+  public static List<Employee> readEmployeeCSV() throws IOException, ParseException {
+    // System.out.println("beginning to read csv");
+
+    Scanner lineScanner =
+            new Scanner(ReadCSV.class.getClassLoader().getResourceAsStream("Employee.csv"));
+    Scanner dataScanner;
+    int dataIndex = 0;
+    int lineIndex = 0;
+    int intData = 0;
+    List<Employee> list = new ArrayList<>();
+    lineScanner.nextLine();
+
+    while (lineScanner.hasNextLine()) { // Scan CSV line by line
+
+      dataScanner = new Scanner(lineScanner.nextLine());
+      dataScanner.useDelimiter(",");
+      Employee thisEmployee = new Employee();
+
+      while (dataScanner.hasNext()) {
+
+        String data = dataScanner.next();
+        if (dataIndex == 0) thisEmployee.setEmployeeID(data);
+        else if (dataIndex == 1) thisEmployee.setEmployeeType(data);
+        else if (dataIndex == 2) thisEmployee.setFirstName(data);
+        else if (dataIndex == 3) thisEmployee.setLastName(data);
+        else if (dataIndex == 4) thisEmployee.setEmail(data);
+        else if (dataIndex == 5) thisEmployee.setPhoneNum(data);
+        else if (dataIndex == 6) thisEmployee.setAddress(data);
+        else if (dataIndex == 7) {
+          Date date = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse(data);
+          thisEmployee.setStartDate(date);
+        } else System.out.println("Invalid data, I broke::" + data);
+        dataIndex++;
+      }
+
+      dataIndex = 0;
+      list.add(thisEmployee);
+      // System.out.println(thisLocation);
+
+    }
+
+    lineIndex++;
+    lineScanner.close();
+    return list;
+  }
+
+  //Read From MedicalEquipment CSV
+  public static List<MedicalEquipment> readMedicalEquipmentCSV() throws IOException, ParseException {
+    // System.out.println("beginning to read csv");
+
+    Scanner lineScanner =
+            new Scanner(ReadCSV.class.getClassLoader().getResourceAsStream("MedicalEquipment.csv"));
+    Scanner dataScanner;
+    int dataIndex = 0;
+    int lineIndex = 0;
+    int intData = 0;
+    List<MedicalEquipment> list = new ArrayList<>();
+    lineScanner.nextLine();
+
+    while (lineScanner.hasNextLine()) { // Scan CSV line by line
+
+      dataScanner = new Scanner(lineScanner.nextLine());
+      dataScanner.useDelimiter(",");
+      MedicalEquipment thisME = new MedicalEquipment();
+
+      while (dataScanner.hasNext()) {
+
+        String data = dataScanner.next();
+        if (dataIndex == 0) thisME.setEquipmentID(data);
+        else if (dataIndex == 1) thisME.setEquipmentType(data);
+        else if (dataIndex == 2) {
+          Boolean boolData = Boolean.parseBoolean(data);
+          thisME.setIsClean(boolData);
+        } else if (dataIndex == 3) thisME.setCurrentLocation(data);
+        else if (dataIndex == 4) {
+          Boolean boolData = Boolean.parseBoolean(data);
+          thisME.setIsClean(boolData);
+          thisME.setIsAvailable(boolData);
+        } else System.out.println("Invalid data, I broke::" + data);
+        dataIndex++;
+      }
+
+      dataIndex = 0;
+      list.add(thisME);
+      // System.out.println(thisLocation);
+
+    }
+
+    lineIndex++;
+    lineScanner.close();
+    return list;
+  }
+
+  //Read from Location CSV
+  public static List<MedicalEquipmentServiceRequest> readMedicalEquipmentServiceRequestCSV() throws IOException, ParseException {
+    // System.out.println("beginning to read csv");
+
+    Scanner lineScanner =
+            new Scanner(ReadCSV.class.getClassLoader().getResourceAsStream("MedicalEquipmentServiceRequest.csv"));
+    Scanner dataScanner;
+    int dataIndex = 0;
+    int lineIndex = 0;
+    int intData = 0;
+    List<MedicalEquipmentServiceRequest> list = new ArrayList<>();
+    lineScanner.nextLine();
+
+    while (lineScanner.hasNextLine()) { // Scan CSV line by line
+
+      dataScanner = new Scanner(lineScanner.nextLine());
+      dataScanner.useDelimiter(",");
+      MedicalEquipmentServiceRequest thisMESR = new MedicalEquipmentServiceRequest();
+
+      while (dataScanner.hasNext()) {
+
+        String data = dataScanner.next();
+        if (dataIndex == 0) thisMESR.setRequestID(data);
+        else if (dataIndex == 1) thisMESR.setStartLocation(data);
+        else if (dataIndex == 2) thisMESR.setEndLocation(data);
+        else if (dataIndex == 3) thisMESR.setEmployeeRequested(data);
+        else if (dataIndex == 4) thisMESR.setEmployeeAssigned(data);
+        else if (dataIndex == 5) {
+          SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+          Date parsedDate = dateFormat.parse(data);
+          Timestamp timestamp = new java.sql.Timestamp(parsedDate.getTime());
+          thisMESR.setRequestTime(timestamp);
+        }
+        else if (dataIndex == 6) thisMESR.setRequestStatus(data);
+        else if (dataIndex == 7) thisMESR.setEquipmentID(data);
+        else if (dataIndex == 8) thisMESR.setRequestType(data);
+        else System.out.println("Invalid data, I broke::" + data);
+        dataIndex++;
+      }
+
+      dataIndex = 0;
+      list.add(thisMESR);
+      // System.out.println(thisLocation);
+
+    }
+
+    lineIndex++;
+    lineScanner.close();
+    return list;
+  }
+
 }
--- src/main/java/edu/wpi/teama/Main.java
+++ src/main/java/edu/wpi/teama/Main.java
@@ -16,10 +16,10 @@ public class Main {
   // code. For now, the aapp.launch is blocking, so can't add it to the main main function.
   public static void main_db(String[] args) throws Exception {
     System.out.println("calling CSV");
-    List<Location> locList = ReadCSV.readCSV();
+    //List<Location> locList = ReadCSV.readCSV();
 
     Connection connection = null;
-    Adb.initialConnection(locList);
+    //db.initialConnection(locList);
     // App.launch(App.class, args);
 
     return;
