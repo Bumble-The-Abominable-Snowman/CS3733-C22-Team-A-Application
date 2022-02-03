c257a423c49dcdf413e2b798f9d389780776df71
Lucy Bernard <lucycbernard@gmail.com>
2022-02-03 10:26:27 -0500

    Updated Lang Services page functionality.

--- src/main/java/edu/wpi/teama/controllers/ServiceRequestControllers/LanguageInterpreterController.java
+++ src/main/java/edu/wpi/teama/controllers/ServiceRequestControllers/LanguageInterpreterController.java
@@ -1,34 +1,80 @@
 package edu.wpi.teama.controllers.ServiceRequestControllers;
 
+import edu.wpi.teama.Adb.Location.Location;
+import edu.wpi.teama.Adb.Location.LocationDerbyImpl;
 import edu.wpi.teama.controllers.SceneController;
+import edu.wpi.teama.entities.LanguageInterpreterRequest;
+import java.io.IOException;
+import java.util.stream.Collectors;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class LanguageInterpreterController extends GenericServiceRequestsController {
-  @FXML private Button floor1Button;
-  @FXML private Button floor2Button;
-  @FXML private Button floor3Button;
-  @FXML private Button floor4Button;
   @FXML private Button returnHomeButton;
   @FXML private Button submitButton;
   @FXML private Button backButton;
   @FXML private Button clearButton;
-  @FXML private ChoiceBox<String> displayLang;
-  @FXML private ChoiceBox<String> langInterpreter;
-  @FXML private ChoiceBox<String> langInterpreter1;
-  @FXML private TextField roomNum;
-  @FXML private TextField commentsBox;
+  @FXML private ComboBox<String> employeeChoice;
+  @FXML private ComboBox<String> toLocationChoice;
+  @FXML private ComboBox<String> languageChoice;
+  @FXML private TextArea commentsBox;
 
   private FXMLLoader loader = new FXMLLoader();
 
   @FXML
   public void initialize() {
     sceneID = SceneController.SCENES.LANGUAGE_INTERPRETER_SERVICE_REQUEST_SCENE;
+
+    commentsBox.setWrapText(true);
+
+    languageChoice.getItems().removeAll(languageChoice.getItems());
+    languageChoice
+        .getItems()
+        .addAll(
+            "American Sign Language",
+            "Arabic",
+            "French",
+            "German",
+            "Italian",
+            "Japanese",
+            "Korean",
+            "Mandarin",
+            "Russian",
+            "Spanish");
+    languageChoice.getSelectionModel().select("Language");
+    languageChoice.setVisibleRowCount(5);
+
+    toLocationChoice.getItems().removeAll(toLocationChoice.getItems());
+    toLocationChoice
+        .getItems()
+        .addAll(
+            new LocationDerbyImpl()
+                .getNodeList().stream().map(Location::getShortName).collect(Collectors.toList()));
+    toLocationChoice.setVisibleRowCount(5);
+
+    employeeChoice.getItems().removeAll(employeeChoice.getItems());
+    employeeChoice.getItems().addAll("Placeholder");
+    employeeChoice.getSelectionModel().select("Employee");
+    employeeChoice.setVisibleRowCount(5);
   }
 
   @FXML
-  void submitRequest() {}
+  private void createLanguageRequest(LanguageInterpreterRequest langRequest) {
+    langRequest.setLanguage(languageChoice.getValue());
+    langRequest.setToLocation(toLocationChoice.getValue());
+    langRequest.setRequestedEmployee(employeeChoice.getValue());
+    langRequest.setToLocation(commentsBox.getText());
+  }
+
+  @FXML
+  void submitRequest() throws IOException {
+    LanguageInterpreterRequest langRequest = new LanguageInterpreterRequest();
+    if (!languageChoice.getSelectionModel().getSelectedItem().equals("Language")
+        && toLocationChoice.getSelectionModel().getSelectedItem() != null
+        && !employeeChoice.getSelectionModel().getSelectedItem().equals("Employee")) {
+      this.createLanguageRequest(langRequest);
+      this.returnToHomeScene();
+    }
+  }
 }
