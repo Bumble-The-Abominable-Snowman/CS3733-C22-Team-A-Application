c9fdc846062bd7719ed6b9724e769d0dfb512f93
RobbieEskridge <robceskridge@gmail.com>
2022-02-03 14:13:02 -0500

    religious barebones update

--- src/main/java/edu/wpi/teama/controllers/ServiceRequestControllers/ReligiousServicesController.java
+++ src/main/java/edu/wpi/teama/controllers/ServiceRequestControllers/ReligiousServicesController.java
@@ -1,30 +1,69 @@
 package edu.wpi.teama.controllers.ServiceRequestControllers;
 
-import edu.wpi.teama.controllers.SceneController;
+import edu.wpi.teama.Aapp;
+import java.io.IOException;
+import java.net.URL;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-public class ReligiousServicesController extends GenericServiceRequestsController {
-  @FXML private TextField religiousDesc = new TextField();
-  @FXML private ChoiceBox religionChoice = new ChoiceBox();
-  @FXML private ChoiceBox fromRoom = new ChoiceBox();
+public class ReligiousServicesController {
+  @FXML private TextArea specialNotes = new TextArea();
+  @FXML private ChoiceBox religionChoiceBox = new ChoiceBox();
+  @FXML private ChoiceBox toChoiceBox = new ChoiceBox();
+  @FXML private ChoiceBox employeeChoiceBox = new ChoiceBox();
+  @FXML private ChoiceBox fromChoiceBox = new ChoiceBox();
+  @FXML private Label locationLabel = new Label();
   @FXML private Button homeButton = new Button();
   @FXML private Button backButton = new Button();
   @FXML private Button clearButton;
-  @FXML private Button submitRequestButton = new Button();
+  @FXML private Button submitButton = new Button();
 
   private FXMLLoader loader = new FXMLLoader();
 
   @FXML
-  public void initialize() {
-    sceneID = SceneController.SCENES.RELIGIOUS_SERVICE_REQUEST_SCENE;
+  private void returnToHomeScene() throws IOException {
+    FXMLLoader loader = new FXMLLoader();
+    URL xmlUrl = Aapp.class.getResource("views/home.fxml");
+    loader.setLocation(xmlUrl);
+    Parent root = loader.load();
+
+    Stage window = (Stage) homeButton.getScene().getWindow();
+    window.setScene(new Scene(root));
+    window.setTitle("Home");
+    window.show();
   }
 
   @FXML
-  void submitRequest() {
+  private void returnToSelectServiceScene() throws IOException {
+    FXMLLoader loader = new FXMLLoader();
+    URL xmlUrl = Aapp.class.getResource("views/selectServiceRequest.fxml");
+    loader.setLocation(xmlUrl);
+    Parent root = loader.load();
+
+    Stage window = (Stage) backButton.getScene().getWindow();
+    window.setScene(new Scene(root));
+    window.setTitle("Select Service Request");
+    window.show();
+  }
+
+  @FXML
+  private void submitRequest() {
     // send request to database
+  }
+
+  @FXML
+  private void clearSubmission() throws IOException {
+    URL xmlUrl = Aapp.class.getResource("views/religiousServices.fxml");
+    loader.setLocation(xmlUrl);
+    Parent root = loader.load();
+
+    Stage window = (Stage) clearButton.getScene().getWindow();
+    window.setScene(new Scene(root));
+    window.setTitle("Religious Requests");
+    window.show();
   }
 }
