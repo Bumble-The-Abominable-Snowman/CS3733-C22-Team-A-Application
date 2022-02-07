02170d1186a7d1ea7fce3478253b681f73f1c029
RobbieEskridge <robceskridge@wpi.edu>
2022-02-07 17:35:42 -0500

    preliminary UI setup

--- src/main/java/edu/wpi/cs3733/c22/teamA/controllers/LogInController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamA/controllers/LogInController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
--- src/main/java/edu/wpi/cs3733/c22/teamA/controllers/map/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamA/controllers/map/MapEditorController.java
@@ -1,13 +1,43 @@
 package edu.wpi.cs3733.c22.teamA.controllers.map;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Polygon;
 
 public class MapEditorController {
 
+  @FXML JFXButton groundFloorButton = new JFXButton();
+  @FXML JFXButton lowerFloorOneButton = new JFXButton();
+  @FXML JFXButton lowerFloorTwoButton = new JFXButton();
+  @FXML JFXButton floorOneButton = new JFXButton();
+  @FXML JFXButton floorTwoButton = new JFXButton();
+  @FXML JFXButton floorThreeButton = new JFXButton();
+  @FXML JFXButton floorFourteenButton = new JFXButton();
+
+  @FXML JFXButton newLocButton = new JFXButton();
+  @FXML JFXButton viewTableButton = new JFXButton();
+
+  @FXML JFXButton saveButton = new JFXButton();
+  @FXML JFXButton clearButton = new JFXButton();
+
+  @FXML JFXButton backButton = new JFXButton();
+  @FXML JFXButton homeButton = new JFXButton();
+
+
   @FXML
   public void initialize() {
     Polygon polygon = new Polygon();
     polygon.getPoints().addAll(new Double[] {1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0});
+  }
+
+  public void clearSubmission(){
+
+  }
+
+  public void returnToHomeScene(){
+
+  }
+  public void returnToSelectServiceScene(){
+
   }
 }
