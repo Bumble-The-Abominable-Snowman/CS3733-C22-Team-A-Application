1ea80751617582a8937495fd3e3b2a792c75aa99
Alexander Sun <technoal9@gmail.com>
2022-02-06 21:59:55 -0500

    draws diamond on floor selection

--- src/main/java/edu/wpi/cs3733/c22/teamA/controllers/map/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamA/controllers/map/MapEditorController.java
@@ -1,13 +1,49 @@
 package edu.wpi.cs3733.c22.teamA.controllers.map;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
 public class MapEditorController {
+  @FXML private ComboBox floorSelectionComboBox;
+  @FXML private AnchorPane anchorPane;
+  private List<Polygon> locationMarkers;
+
+  public MapEditorController() {
+    locationMarkers = new ArrayList<>();
+  }
 
   @FXML
   public void initialize() {
-    Polygon polygon = new Polygon();
-    polygon.getPoints().addAll(new Double[] {1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 1.0, 1.0, 1.0});
+    floorSelectionComboBox.getItems().removeAll(floorSelectionComboBox.getItems());
+    floorSelectionComboBox
+        .getItems()
+        .addAll("Choose Floor:", "Floor 1", "Floor 2", "Floor 3", "L1", "L2", "Ground");
+    floorSelectionComboBox.getSelectionModel().select("Choose Floor:");
+    floorSelectionComboBox
+        .getSelectionModel()
+        .selectedItemProperty()
+        .addListener(
+            (obs, oldValue, newValue) -> {
+              if (newValue.equals("Choose Floor:")) {
+
+              } else if (newValue.equals("Floor 1")) {
+                Polygon polygon = new Polygon();
+                polygon
+                    .getPoints()
+                    .addAll(new Double[] {100.0, 110.0, 92.5, 100.0, 100.0, 90.0, 107.5, 100.0});
+                polygon.setFill(Color.color(0, 0.6, 1.0));
+                locationMarkers.add(polygon);
+                draw();
+              }
+            });
+  }
+
+  private void draw() {
+    anchorPane.getChildren().add(locationMarkers.get(0));
   }
 }
