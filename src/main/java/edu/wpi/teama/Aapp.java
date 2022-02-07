2c3ae6f80ece23e1fc6b21be713c3c9947741859
Yaşar İdikut <yasar.idikut@hisarschool.k12.tr>
2022-02-07 13:29:08 -0500

    implemented authentication

--- src/main/java/edu/wpi/teama/Aapp.java
+++ src/main/java/edu/wpi/teama/Aapp.java
@@ -1,5 +1,7 @@
 package edu.wpi.teama;
 
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 import edu.wpi.teama.controllers.SceneController;
 import java.io.IOException;
 import javafx.application.Application;
@@ -12,6 +14,9 @@ public class Aapp extends Application {
   private static Stage guiStage;
   public static SceneController sceneController;
 
+  public static ConnectionFactory factory = new ConnectionFactory();
+  public static Connection connection;
+
   public static Stage getStage() {
     return guiStage;
   }
@@ -19,7 +24,9 @@ public class Aapp extends Application {
   @Override
   public void init() throws IOException {
     log.info("Starting Up");
-    // test
+
+    Aapp.factory.setHost("198.199.83.208");
+    Aapp.factory.setPort(5672); // 5672 for regular connections, 5671 for connections that use TLS
   }
 
   @Override
--- src/main/java/edu/wpi/teama/controllers/LogInController.java
+++ src/main/java/edu/wpi/teama/controllers/LogInController.java
@@ -1,37 +1,100 @@
 package edu.wpi.teama.controllers;
 
+import com.rabbitmq.client.AuthenticationFailureException;
 import edu.wpi.teama.Aapp;
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 
 public class LogInController {
+  @FXML private Text welcomeBox;
   @FXML private TextField usernameBox;
-  @FXML private TextField passwordBox;
+  @FXML private PasswordField passwordBox;
   @FXML private Button loginButton;
   @FXML private Button exitButton;
-  private FXMLLoader loader = new FXMLLoader();
+
+  private String incomingMessage;
 
   private final SceneController sceneController = Aapp.sceneController;
 
   @FXML
-  private void logIn(ActionEvent actionEvent) throws IOException {
-    String username = usernameBox.getText();
-    String password = passwordBox.getText();
+  private void logIn(ActionEvent actionEvent) throws IOException, TimeoutException {
+    Aapp.factory.setUsername(usernameBox.getText());
+    Aapp.factory.setPassword(passwordBox.getText());
 
-    // Here would be where we implement the database access portion, but haven't met yet so
-    // placeholder
+    try {
+      Aapp.connection = Aapp.factory.newConnection("app:audit component:event-consumer");
 
-    System.out.println("LOG IN");
+      welcomeBox.setText("Success!");
+      welcomeBox.setFill(Color.GREEN);
 
-    sceneController.switchScene(SceneController.SCENES.HOME_SCENE);
+      sceneController.switchScene(SceneController.SCENES.HOME_SCENE);
+    } catch (AuthenticationFailureException exception) {
+
+      welcomeBox.setText("Unsuccesful login!");
+      welcomeBox.setFill(Color.RED);
+
+      usernameBox.setText("");
+      passwordBox.setText("");
+    }
   }
 
   @FXML
   private void exitApp(ActionEvent actionEvent) {
     System.exit(0);
   }
+
+  @FXML
+  public void initialize() throws IOException, TimeoutException {
+
+    //    Task task =
+    //            new Task<Void>() {
+    //              @Override
+    //              protected Void call() throws Exception {
+    //                while (true) {
+    //                  Platform.runLater(
+    //                          new Runnable() {
+    //                            @Override
+    //                            public void run() {
+    //                              messageLabel.setText("incomingMessage: " + incomingMessage);
+    //                            }
+    //                          });
+    //
+    //                  TimeUnit.MILLISECONDS.sleep(100);
+    //                }
+    //              }
+    //            };
+    //    new Thread(task).start();
+    //
+    //    DeliverCallback deliverCallback =
+    //            (consumerTag, delivery) -> {
+    //              incomingMessage = new String(delivery.getBody(), StandardCharsets.UTF_8);
+    //              System.out.println(" [x] Received '" + incomingMessage + "'");
+    //            };
+    //    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+  }
+
+  //  @FXML
+  //  private void submitRequest() throws IOException, TimeoutException {
+  //    System.out.print("\nNew request, got some work to do bud!\n");
+  //    System.out.printf(
+  //            "Added this note : \n[NOTE START]\n%s\n[NOTE END]\n", specialNotes.getCharacters());
+  //
+  //    try (Connection connection = factory.newConnection();
+  //         Channel channel = connection.createChannel()) {
+  //      channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+  //      String message = specialNotes.getCharacters().toString();
+  //      channel.basicPublish(
+  //              "",
+  //              QUEUE_NAME,
+  //              null,
+  //              specialNotes.getCharacters().toString().getBytes(StandardCharsets.UTF_8));
+  //    }
+  //  }
 }
