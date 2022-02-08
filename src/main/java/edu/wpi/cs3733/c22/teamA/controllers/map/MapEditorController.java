b692b6eaba7a32deaf42ea346b89e6477604e944
RobbieEskridge <robceskridge@wpi.edu>
2022-02-07 19:55:41 -0500

    full add, and edit functionality

--- src/main/java/edu/wpi/cs3733/c22/teamA/controllers/map/MapEditorController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamA/controllers/map/MapEditorController.java
@@ -67,6 +67,7 @@ public class MapEditorController {
   // location object, this function works fine. if each diamond is assigned a location
   // NodeID to save space or smth, can easily be refactored
   public void existingLocSelected(Location currLocation) {
+    nodeIDText.setEditable(false);
     editButton.setDisable(false);
     saveButton.setDisable(true);
     inputVBox.setDisable(false);
@@ -106,24 +107,61 @@ public class MapEditorController {
         l.getNodeType(),
         l.getLongName(),
         l.getShortName());
-    // i dont wanna clutter the database with tests, remove the following line and this works
-    locationBase.deleteLocationNode(l.getNodeID());
   }
 
   public void editLocation() {
-    // selectedLocation = locationBase.getLocationNode(selectedLocation.getNodeID());
-    selectedLocation = locationBase.getLocationNode("rceskridge");
+    // next line may need to be refactored when Alex / Ethan's implementation comes through.
+    // basically, right now it takes the ID of the location selected in the
+    // existingLocSelected function above. Depending on implementation, this may change.
+
+    // also, right now it makes a new, nearly-identical object to the previous one instead of
+    // directly editing, as directly editing resulted in several errors.
+
     String ID = selectedLocation.getNodeID();
-    locationBase.updateLocation(ID, "xCoord", xPosText.getText());
-    locationBase.updateLocation(ID, "yCoord", yPosText.getText());
+
+    Location l = new Location();
+    l.setXCoord(Integer.parseInt(xPosText.getText()));
+    l.setYCoord(Integer.parseInt(yPosText.getText()));
+    l.setBuilding(buildingText.getText());
+    l.setFloor(floorText.getText());
+    l.setNodeType(typeText.getText());
+    l.setLongName(longnameText.getText());
+    l.setShortName(shortnameText.getText());
+
+    locationBase.deleteLocationNode(ID);
+
+    l.setNodeID(ID);
+    locationBase.enterLocationNode(
+        l.getNodeID(),
+        l.getXCoord(),
+        l.getYCoord(),
+        l.getFloor(),
+        l.getBuilding(),
+        l.getNodeType(),
+        l.getLongName(),
+        l.getShortName());
+
+    /*
+    locationBase.updateLocation(ID, "xcoord", xPosText.getText());
+    locationBase.updateLocation(ID, "ycoord", yPosText.getText());
     locationBase.updateLocation(ID, "floor", floorText.getText());
     locationBase.updateLocation(ID, "building", buildingText.getText());
     locationBase.updateLocation(ID, "nodeType", typeText.getText());
     locationBase.updateLocation(ID, "longName", longnameText.getText());
     locationBase.updateLocation(ID, "shortName", shortnameText.getText());
+    */
   }
 
-  public void clearSubmission() {}
+  public void clearSubmission() {
+    nodeIDText.setText("");
+    xPosText.setText("");
+    yPosText.setText("");
+    floorText.setText("");
+    buildingText.setText("");
+    typeText.setText("");
+    longnameText.setText("");
+    shortnameText.setText("");
+  }
 
   @FXML
   public void goToLocationTable() throws IOException {
