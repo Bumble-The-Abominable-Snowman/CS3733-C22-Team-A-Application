48f6ba21abda2154b363cf61fc5026c3bddb3ac2
Lucy Bernard <lucycbernard@gmail.com>
2022-02-08 12:10:49 -0500

    Updated settings page UI.

--- src/main/java/edu/wpi/cs3733/c22/teamA/controllers/settings/SettingsController.java
+++ src/main/java/edu/wpi/cs3733/c22/teamA/controllers/settings/SettingsController.java
@@ -1,20 +1,34 @@
 package edu.wpi.cs3733.c22.teamA.controllers.settings;
 
+import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733.c22.teamA.Aapp;
 import edu.wpi.cs3733.c22.teamA.controllers.SceneController;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 public class SettingsController {
-  @FXML private Button homeButton;
-  @FXML private Button backButton;
-  @FXML private Button loadFromBackupButton;
-  @FXML private Button exportToBackupButton;
+  @FXML private JFXButton backButton;
+  @FXML private JFXButton loadFromBackupButton;
+  @FXML private JFXButton exportToBackupButton;
   private FXMLLoader loader = new FXMLLoader();
   private final SceneController sceneController = Aapp.sceneController;
+
+  @FXML
+  public void initialize() {
+    backButton.setBackground(
+        new Background(new BackgroundFill(Color.LIGHTGRAY, new CornerRadii(0), Insets.EMPTY)));
+    loadFromBackupButton.setBackground(
+        new Background(new BackgroundFill(Color.LIGHTGRAY, new CornerRadii(20), Insets.EMPTY)));
+    exportToBackupButton.setBackground(
+        new Background(new BackgroundFill(Color.LIGHTGRAY, new CornerRadii(20), Insets.EMPTY)));
+  }
 
   public void returnToHomeScene(ActionEvent actionEvent) throws IOException {
     sceneController.switchScene(SceneController.SCENES.HOME_SCENE);
