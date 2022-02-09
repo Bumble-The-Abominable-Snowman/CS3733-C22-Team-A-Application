095f895fc17ce88b8f0e95a4be190ab4220099a2
NYXXMEISTER <55163966+NYXXMEISTER@users.noreply.github.com>
2022-02-08 21:45:12 -0500

    FDSR read and write tested

--- src/test/java/edu/wpi/cs3733/c22/teamA/DefaultTest.java
+++ src/test/java/edu/wpi/cs3733/c22/teamA/DefaultTest.java
@@ -289,50 +289,13 @@ public class DefaultTest {
   public void testLanguage() throws IOException {
     Adb.initialConnection();
 
-        LanguageServiceRequestDerbyImpl.inputFromCSV(
-            "LanguageServiceRequest", "edu/wpi/cs3733/c22/teamA/db/LanguageServiceRequest.csv");
+    LanguageServiceRequestDerbyImpl.inputFromCSV(
+        "LanguageServiceRequest", "edu/wpi/cs3733/c22/teamA/db/LanguageServiceRequest.csv");
 
-        LanguageServiceRequestDerbyImpl derby = new LanguageServiceRequestDerbyImpl();
-        LanguageServiceRequest lsr =
-            new LanguageServiceRequest(
-                "lan124",
-                "start",
-                "end",
-                "emp1",
-                "emp2",
-                "2020-01-01 12:45:00",
-                "In Progress",
-                "High Priority",
-                "no additional comments",
-                "Normal Wash");
-        System.out.println("Testing enter");
-        derby.enterLanguageServiceRequest(lsr);
-        System.out.println("Testing get");
-        LanguageServiceRequest rsr2 = derby.getLanguageServiceRequest("lan123");
-        System.out.println("Got RequestID: " + rsr2.getRequestID());
-        System.out.println("Testing update: updating washMode to 'Super Wash'");
-        derby.updateLanguageServiceRequest("lan123", "language", "Super Wash");
-        LanguageServiceRequest rsr3 = derby.getLanguageServiceRequest("lan123");
-        System.out.println("Got washMode: " + rsr3.getLanguage());
-        System.out.println("Testing getList");
-        List<LanguageServiceRequest> list = derby.getLanguageServiceRequestList();
-        System.out.println("First element language: " + list.get(0).getLanguage());
-        System.out.println("testing delete");
-            derby.deleteLanguageServiceRequest("lan123");
-            LanguageServiceRequestDerbyImpl.exportToCSV(
-                "LanguageServiceRequest",
-                "src/main/resources/edu/wpi/cs3733/c22/teamA/db/LanguageServiceRequest.csv");
-  }
-
-  @Test
-  public void testFoodDelivery() {
-
-    Adb.initialConnection();
-
-    FoodDeliveryServiceRequestDerbyImpl derby = new FoodDeliveryServiceRequestDerbyImpl();
-    FoodDeliveryServiceRequest fdsr =
-        new FoodDeliveryServiceRequest(
-            "fod123",
+    LanguageServiceRequestDerbyImpl derby = new LanguageServiceRequestDerbyImpl();
+    LanguageServiceRequest lsr =
+        new LanguageServiceRequest(
+            "lan124",
             "start",
             "end",
             "emp1",
@@ -341,23 +304,66 @@ public class DefaultTest {
             "In Progress",
             "High Priority",
             "no additional comments",
-            "Steak",
-            "Fries",
-            "Soda",
-            "Cake");
+            "Normal Wash");
     System.out.println("Testing enter");
-    derby.enterFoodDeliveryRequest(fdsr);
+    derby.enterLanguageServiceRequest(lsr);
     System.out.println("Testing get");
-    FoodDeliveryServiceRequest rsr2 = derby.getFoodDeliveryRequest("fod123");
+    LanguageServiceRequest rsr2 = derby.getLanguageServiceRequest("lan123");
     System.out.println("Got RequestID: " + rsr2.getRequestID());
-    System.out.println("Testing update: updating dessert to 'cookie'");
-    derby.updateFoodDeliveryRequest("fod123", "dessert", "cookie");
-    FoodDeliveryServiceRequest rsr3 = derby.getFoodDeliveryRequest("fod123");
-    System.out.println("Got dessert: " + rsr3.getDessert());
+    System.out.println("Testing update: updating washMode to 'Super Wash'");
+    derby.updateLanguageServiceRequest("lan123", "language", "Super Wash");
+    LanguageServiceRequest rsr3 = derby.getLanguageServiceRequest("lan123");
+    System.out.println("Got washMode: " + rsr3.getLanguage());
     System.out.println("Testing getList");
-    List<FoodDeliveryServiceRequest> list = derby.getFoodDeliveryRequestList();
-    System.out.println("First element dessert: " + list.get(0).getDessert());
+    List<LanguageServiceRequest> list = derby.getLanguageServiceRequestList();
+    System.out.println("First element language: " + list.get(0).getLanguage());
     System.out.println("testing delete");
-    derby.deleteFoodDeliveryRequest("fod123");
+    derby.deleteLanguageServiceRequest("lan123");
+    LanguageServiceRequestDerbyImpl.exportToCSV(
+        "LanguageServiceRequest",
+        "src/main/resources/edu/wpi/cs3733/c22/teamA/db/LanguageServiceRequest.csv");
+  }
+
+  @Test
+  public void testFoodDelivery() throws IOException {
+
+    Adb.initialConnection();
+
+    FoodDeliveryServiceRequestDerbyImpl.inputFromCSV(
+        "FoodDeliveryServiceRequest", "edu/wpi/cs3733/c22/teamA/db/FoodDeliveryServiceRequest.csv");
+
+        FoodDeliveryServiceRequestDerbyImpl derby = new FoodDeliveryServiceRequestDerbyImpl();
+        FoodDeliveryServiceRequest fdsr =
+            new FoodDeliveryServiceRequest(
+                "fod123",
+                "start",
+                "end",
+                "emp1",
+                "emp2",
+                "2020-01-01 12:45:00",
+                "In Progress",
+                "High Priority",
+                "no additional comments",
+                "Steak",
+                "Fries",
+                "Soda",
+                "Cake");
+        System.out.println("Testing enter");
+        derby.enterFoodDeliveryRequest(fdsr);
+        System.out.println("Testing get");
+        FoodDeliveryServiceRequest rsr2 = derby.getFoodDeliveryRequest("fod123");
+        System.out.println("Got RequestID: " + rsr2.getRequestID());
+        System.out.println("Testing update: updating dessert to 'cookie'");
+        derby.updateFoodDeliveryRequest("fod123", "dessert", "cookie");
+        FoodDeliveryServiceRequest rsr3 = derby.getFoodDeliveryRequest("fod123");
+        System.out.println("Got dessert: " + rsr3.getDessert());
+        System.out.println("Testing getList");
+        List<FoodDeliveryServiceRequest> list = derby.getFoodDeliveryRequestList();
+        System.out.println("First element dessert: " + list.get(0).getDessert());
+        System.out.println("testing delete");
+        derby.deleteFoodDeliveryRequest("fod123");
+        FoodDeliveryServiceRequestDerbyImpl.exportToCSV(
+            "FoodDeliveryServiceRequest",
+            "src/main/resources/edu/wpi/cs3733/c22/teamA/db/FoodDeliveryServiceRequest.csv");
   }
 }
