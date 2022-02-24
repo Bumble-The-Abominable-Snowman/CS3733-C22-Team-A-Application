f63d962fdda49944ea77f45fe1c28700319ba5d0
ETShrimpinator <jecschrim@gmail.com>
2022-02-24 01:07:53 -0500

    Actually fixed the map this time I promise

--- src/main/java/edu/wpi/cs3733/c22/teamA/controllers/AboutCtrl.java
+++ src/main/java/edu/wpi/cs3733/c22/teamA/controllers/AboutCtrl.java
@@ -1,10 +1,15 @@
 package edu.wpi.cs3733.c22.teamA.controllers;
 
-import edu.wpi.cs3733.c22.teamA.SceneSwitcher;
-import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class AboutCtrl extends MasterCtrl {
 
+    @FXML
+    private void initialize() {
+
+        configure();
+
+    }
 
 }
+
